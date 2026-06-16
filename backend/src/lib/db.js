8588fd63122e2c92ab39e54771432c2c90e477b7
Doves788 <mhrdevesh@gmail.com>
2026-06-16 19:11:19 +0530

    commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,13 +3,11 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const { MONGO_URI } = ENV;
-    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
-
-    const conn = await mongoose.connect(ENV.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://chatuser:ChatApp2026@cluster0.fofryk6.mongodb.net/chatify?appName=Cluster0");
+    
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
     console.error("Error connection to MONGODB:", error);
-    process.exit(1); // 1 status code means fail, 0 means success
+    process.exit(1); 
   }
 };
