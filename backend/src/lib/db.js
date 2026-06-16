4e8d6c6e0820d0bcc6fef787817289206594640d
Doves788 <mhrdevesh@gmail.com>
2026-06-17 03:50:07 +0530

    fix: removed hardcoded db string to use ENV variables

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,7 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://chatuser:ChatApp2026@cluster0.fofryk6.mongodb.net/chatify?appName=Cluster0");
+    const conn = await mongoose.connect(ENV.MONGO_URI);
     
     console.log("MONGODB CONNECTED:", conn.connection.host);
   } catch (error) {
